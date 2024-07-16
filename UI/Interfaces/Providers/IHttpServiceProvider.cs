a048ae4f5f5446d7fb558de9a2dc7f2f40a53b6f
Akbar <Akbar@DESKTOP-16DASBH>
2024-07-16 12:37:51 +0330

    Add HttpServiceProvider

---
+++ UI/Interfaces/Providers/IHttpServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using UI.DTOs.Providers;
+
+namespace UI.Interfaces.Providers
+{
+    public interface IHttpServiceProvider
+    {
+        Task<HttpResponseWraper<TResponse?>> Get<TResponse>(string url);
+        Task<HttpResponseWraper<TResponse?>> Post<T, TResponse>(string url, T data);
+        Task<HttpResponseWraper<TResponse?>> Put<T, TResponse>(string url, T data);
+        Task<HttpResponseWraper<TResponse?>> Delete<TResponse>(string url);
+    }
+}
