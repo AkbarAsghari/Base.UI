d243bf001f89f21ec3e0eed4c0b743b76a399172
Akbar <akbarasghari@outlook.com>
2024-07-20 01:53:11 +0330

    Add ProblemDetails

--- UI/Exceptions/HttpResponseExceptionHander.cs
+++ UI/Exceptions/HttpResponseExceptionHander.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 using MudBlazor;
 using System;
 using System.Net;
 using System.Text.Json;
-using UI.DTOs.Repositories.Exceptions;
+using UI.Enums;
 
 namespace UI.Exceptions
 {
@@ -20,7 +21,7 @@ namespace UI.Exceptions
 
                 switch (statusCode)
                 {
-               
+
                     case HttpStatusCode.Forbidden:
                     case HttpStatusCode.BadRequest:
                     case HttpStatusCode.Conflict:
@@ -48,13 +49,19 @@ namespace UI.Exceptions
             if (String.IsNullOrEmpty(content))
                 return false;
 
-            var error = JsonSerializer.Deserialize<ExceptionDTO>(content!, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            var error = JsonSerializer.Deserialize<Propblem>(content!, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             if (error != null)
             {
-                _Snackbar.Add(error.Key, Severity.Error);
+                _Snackbar.Add(message: error.Messages == null ? error.Detail : error.Messages[ExceptionLanguageEnum.Fa].ToString(),
+                    severity: (error.Status >= 400 && error.Status < 500 ? Severity.Warning : error.Status >= 500 ? Severity.Error : Severity.Info));
                 return true;
             }
             return false;
         }
+    }
+
+    class Propblem : ProblemDetails
+    {
+        public Dictionary<ExceptionLanguageEnum, string>? Messages { get; set; }
     }
 }
